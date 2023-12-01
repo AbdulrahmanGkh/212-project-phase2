bd60d0ed57afa954512bb004649ffece9b8a466b
AbdulrahmanGkh <146248943+AbdulrahmanGkh@users.noreply.github.com>
2023-12-01 18:55:49 +0300

    Update phoneBook.java

--- 212 project phase2/src/phoneBook.java
+++ 212 project phase2/src/phoneBook.java
@@ -1,4 +1,67 @@
 
-public class phoneBook {
+public class PhoneBook {
+	
+
+	public static void ProgramMenu() {  //we must add defult case if the user enters wrong number
+	System.out.println("Welcome to the Linked Tree Phoonebook!");
+	System.out.println("Please choose an option");
+	System.out.println("1.Add a contact");
+	System.out.println("2.Search for a contact");
+	System.out.println("3.Delete a contact");
+	System.out.println("4.Sechedule an event");
+	System.out.println("5.Print event details");
+	System.out.println("6.Print contact by first name");
+	System.out.println("7.Print all events alphabetically");
+	System.out.println("8.Print Contact that share an event");
+	System.out.println("9.Exit");
+	System.out.println("Enter your choice: ");
+	}
+	
+	public static void SearchContactMenu() {
+		System.out.println("Enter Search criteria: ");
+		System.out.println("Name");
+		System.out.println("Phone Number");
+		System.out.println("Email Address");
+		System.out.println("Address");
+		System.out.println("Birthday");
+	}
+	
+	public static void PrintEvent() {
+		System.out.println("Enter search criteria");
+		System.out.println("1. contact name");
+		System.out.println("2. Event tittle");
+		System.out.println("Enter your choice");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
